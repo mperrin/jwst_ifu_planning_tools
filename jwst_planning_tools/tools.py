192d390416a90e6749e0e9f50584f055ec4b9f44
Polychronis Patapis <chronispatapis@gmail.com>
2024-08-26 13:29:10 +0200

    added webbpsf functionality - webbpsf not compatible yet though.

--- jwst_planning_tools/tools.py
+++ jwst_planning_tools/tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from scipy.ndimage import shift
 from matplotlib.collections import PatchCollection
 import pandas as pd
 from astropy.io import fits
@@ -8,7 +9,7 @@ import astropy.units as u
 
 #### MRS ###
 
-fov_MRS = {"1": (3.3,3.7), "2":(4.0 , 4.8)}
+fov_MRS = {"1": (3.3,3.7), "2":(4.0 , 4.8), "3":(5.6, 6.2)}
 dither_off = {"neg": [(-1.078, 0.528), (0.98, -0.44), (1.078, -0.528), (-0.980, 0.44)],
               "pos": [(1.078, 0.528), (-0.98, -0.44), (-1.078, -0.528), (0.980, 0.44)],
               "ext":[(0.4*0.18, 0.4*0.18), (-0.4*0.18, 0.4*0.18), (0.4*0.18,-0.4*0.18), (-0.4*0.18,-0.4*0.18)],
@@ -108,7 +109,7 @@ def v2v3_to_XidlYidl(v2, v3):
 
 
 def simulate_geometry(planets, v3pa, band, which, sign, offset=None, system_name=None, primary=None,
-                      webbpsf=False):
+                      webbpsf_plot=False):
     """
 
     :param planets: list of tuples with (separation, pa, flux (optional)) for each planet
@@ -124,19 +125,27 @@ def simulate_geometry(planets, v3pa, band, which, sign, offset=None, system_name
     """
     nplanets = np.shape(planets)[0]
     coordinates = np.zeros((nplanets+1, 2))
+    contrasts = np.ones(nplanets+1)
     for i, p in enumerate(planets):
-        separation, pa, _ = p
+        separation, pa, contrast = p
         mrspa = pa_to_MRS_pa(pa, v3pa, band)
         beta, alpha = separation * np.cos(-mrspa * np.pi / 180), separation * np.sin(-mrspa * np.pi / 180)
         coordinates[i+1, 0] = alpha
         coordinates[i+1, 1] = beta
+        contrasts[i+1] = contrast
 
     if primary is not None:
         try:
             coordinates[:, 0] -= coordinates[primary, 0]
             coordinates[:, 1] -= coordinates[primary, 1]
+            contrasts[:] /= contrasts[primary]
         except IndexError:
             print("Invalid index for planet. Setting star as primary")
+    else:
+        try:
+            contrasts[:] /= contrasts[1]
+        except IndexError:
+            print("No planet in list.")
 
     if offset is not None:
         coordinates[:, 0] -= offset[0]
@@ -169,10 +178,66 @@ def simulate_geometry(planets, v3pa, band, which, sign, offset=None, system_name
     plt.ylabel("MRS beta [arcsec]")
     plt.show()
 
-    if webbpsf:
+    if webbpsf_plot:
+        simfov = 2*fov_MRS[band[0]][0]
+        miri = webbpsf.MIRI()
+        miri.image_mask = "MIRI-IFU_3"
+        miri.filter = "DSHORT"
+        band = "3A"
+        xoff = coordinates[0, 0]
+        yoff = coordinates[0, 1]
+        miri.options['source_offset_x'] = xoff
+        miri.options['source_offset_y'] = yoff
         # produce PSF for each object given fluxes in Jy
-        pass
+        starpsf = miri.calc_psf(monochromatic=miri.wavelength, outfile=None, add_distortion=False,
+                                fov_arcsec=simfov)
+
+        miri.options['source_offset_x'] = xoff + np.random.normal(0, 0.1*miri._IFU_pixelscale[f"Ch{band[0]}"][1])
+        miri.options['source_offset_y'] = yoff + np.random.normal(0, 0.1*miri._IFU_pixelscale[f"Ch{band[0]}"][1])
+        # produce PSF for each object given fluxes in Jy
+        refpsf = miri.calc_psf(monochromatic=miri.wavelength, outfile=None, add_distortion=False,
+                                fov_arcsec=simfov)
+        print("Placing star at ", miri.options['source_offset_x'], miri.options['source_offset_y'])
+        xoff = coordinates[1, 0]
+        yoff = coordinates[1, 1]
+        miri.options['source_offset_x'] = xoff
+        miri.options['source_offset_y'] = yoff
+        print("Placing planet at ", coordinates[1, :])
+        planetpsf = miri.calc_psf(monochromatic=miri.wavelength, outfile=None, add_distortion=False,
+                                fov_arcsec=simfov)
+        # ref_psf = shift(starpsf[3].data, shift=np.random.normal(0, 0.1, size=2))
+        plt.figure()
+
+        plt.imshow(starpsf[3].data*10000+planetpsf[3].data, origin="lower", cmap="plasma", vmax=1,
+                   extent=[-simfov/2, simfov/2, -simfov/2, simfov/2])
+        plt.scatter(coordinates[1, 0], coordinates[1, 1], marker="+", color=f"red")
+        dither_pattern(plt.gca(), [0., 0.], sign=sign, ch=band[0], which=which, color="white")
+        plt.gca().set_aspect('equal')
+        plt.suptitle(f"V3 PA: {v3pa} - Science")
+        plt.xlabel("MRS alpha [arcsec]")
+        plt.ylabel("MRS beta [arcsec]")
+        plt.show()
+
+        plt.figure()
+
+        plt.imshow(starpsf[3].data * contrasts[0] + planetpsf[3].data - refpsf[3].data*contrasts[0], origin="lower",
+                   cmap="RdBu_r", vmin=-0.1, vmax=0.1,
+                   extent=[-simfov / 2, simfov / 2, -simfov / 2, simfov / 2])
+        plt.scatter(coordinates[1, 0], coordinates[1, 1], marker="+", color=f"red")
+        dither_pattern(plt.gca(), [0., 0.], sign=sign, ch=band[0], which=which, color="white")
+        plt.gca().set_aspect('equal')
+        plt.suptitle(f"V3 PA: {v3pa} - Residuals")
+        plt.xlabel("MRS alpha [arcsec]")
+        plt.ylabel("MRS beta [arcsec]")
+        plt.show()
 
 if __name__=="__main__":
-    simulate_geometry(planets=[(1.2, 30, 6*1e-3), (2.4, 30, 6*1e-4)], v3pa=120, band="1A",
-                      which="4pt", sign="neg", offset=None, system_name="YSES1-", primary=2)
+
+    import sys
+
+    sys.path.append("/Users/polychronispatapis/Documents/PhD/MIRI/MRS_PSF/paper_analysis/")
+    from optimise_cube_webbpsf import *
+
+
+    simulate_geometry(planets=[(4.1, 37.4, 6*1e-3)], v3pa=60, band="3A",
+                      which="4pt", sign="neg", offset=None, system_name="Tatooine-", primary=1, webbpsf_plot=True)
