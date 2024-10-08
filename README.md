4871c40ffda62a3901fe898e89ade0cc1a598f56
Polychronis Patapis <chronispatapis@gmail.com>
2024-10-08 10:30:54 +0200

    added test and checked installation

--- README.md
+++ README.md
@@ -5,13 +5,20 @@ Convenience functions to plan JWST observations
 Clone the repository
 ```
 git clone https://github.com/patapisp/jwst_planning_tools.git
-cd jwst_planning_tools/
 ```
 Dependencies
 - webbpsf development branch: https://github.com/spacetelescope/webbpsf.git
 - miricoord package: https://github.com/STScI-MIRI/miricoord
 
-Then run
+For miricoord you need to follow the instructions on the gitgub repo. In your python environment, clone the repository 
+and run
+```commandline
+python setup.py install
 ```
+Then continue the jwst planning tools installation and run
+```
+cd jwst_planning_tools/
 pip install -e .
 ```
+Be sure to also point to the webbpsf data by downloading them from https://webbpsf.readthedocs.io/en/stable/installation.html#installing-the-required-data-files
+and exporting the "WEBBPSF_PATH" environment variable.
--- jwst_planning_tools/tests/test_base.py
+++ jwst_planning_tools/tests/test_base.py
@@ -1,6 +1,9 @@
 from jwst_planning_tools.tools import mrs_planning_tool
 from jwst_planning_tools.util import get_v3PA_range
 import numpy as np
+import webbpsf
+import os
+from miricoord.mrs import mrs_tools as mt
 
 # test getting the coordinates and V3 PA range
 
@@ -8,9 +11,26 @@ def test_get_V3PA_from_target():
     target_name = "HR 8799"
     _, _, _, _, V3PA_range = get_v3PA_range(target_name=target_name, ra=None, dec=None, start=None, end=None,
                                             cycle=3)
+    # print(~np.isnan(np.array(V3PA_range)).any())
+    assert ~np.isnan(np.array(V3PA_range)).any()
 
-    assert np.isnan(V3PA_range).any()
+def test_webbpsf_MRS():
+    miri = webbpsf.MIRI()
+    miri.mode = "IFU"
+    miri.options['ifu_broadening'] = "empirical"
+    miri.include_si_wfe = False
+    miri.band = "1A"
+    miri.options['source_offset_x'] = 0.
+    miri.options['source_offset_y'] = 0.
+    psf = miri.calc_psf(monochromatic=5.3*1e-6, oversample=7, fov_arcsec=6, add_distortion=True)
+    print(miri.pixelscale, miri.band, miri.aperturename, miri._get_aperture_rotation(miri.aperturename),
+          miri._get_pixelscale_from_apername(miri.aperturename))
+
+def test_miricoord_transform():
+    print(mt.v2v3_to_xyideal(-503.378, -318.999))
 
 
 if __name__ == "__main__":
     test_get_V3PA_from_target()
+    test_webbpsf_MRS()
+    test_miricoord_transform()
