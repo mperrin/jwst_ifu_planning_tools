fixed 2 bugs: - sign on MRS PA - V3 PA range > 2

jwst_planning_tools/tools.py
@@ -82,7 +82,7 @@ def create_dithered_scene(planet_angles, planet_sep, v3_pa, planet_names, dither
     for i, r in enumerate(planet_sep):
         v3PA = np.linspace(v3_pa[0], v3_pa[1], endpoint=True)
         MRSPAS = pa_to_MRS_pa(pa=planet_angles[i], v3pa=v3PA, band=band)
-        beta, alpha = planet_sep[i] * np.cos(-MRSPAS * np.pi / 180), planet_sep[i] * np.sin(-MRSPAS * np.pi / 180)
+        beta, alpha = planet_sep[i] * np.cos(MRSPAS * np.pi / 180), planet_sep[i] * np.sin(MRSPAS * np.pi / 180)
         ax.scatter(alpha, beta, label=planet_names[i])
 
         dither_pattern(ax=ax, center=[np.median(alpha), np.median(beta)], sign=dither_sign, which=which, ch=band[0])

jwst_planning_tools/util.py
@@ -49,10 +49,10 @@ def get_v3PA_range(target_name, ra=None, dec=None, start=None, end=None, cycle=4
         return None
 
     min_PA_data, max_PA_data = [], []
+    v3pa_range = []
     for starti, endi in window_indices:
         data_to_plot = df.loc[starti:endi]
-        min_PA_data.append(np.round(np.min(data_to_plot['V3PA_min_pa_angle']), 1))
-        max_PA_data.append(np.round(np.max(data_to_plot['V3PA_max_pa_angle']), 1))
-
-    v3pa_range = (min_PA_data, max_PA_data)
+        min_PA_data = np.round(np.min(data_to_plot['V3PA_min_pa_angle']), 1)
+        max_PA_data = np.round(np.max(data_to_plot['V3PA_max_pa_angle']), 1)
+        v3pa_range.append([min_PA_data, max_PA_data])
     return start, end, ra, dec, v3pa_range